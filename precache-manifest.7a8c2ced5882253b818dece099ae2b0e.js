f0b1b00d93b39b09427fa2bb35c8c59a28c5a912
Olavur Ellefsen <oe@tokni.com>
2019-01-25 14:36:46 +0000

    Updates

---
+++ precache-manifest.7a8c2ced5882253b818dece099ae2b0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "0208acf33aabeb631a5b",
+    "url": "/react-tutorial/static/js/main.0208acf3.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-tutorial/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "0208acf33aabeb631a5b",
+    "url": "/react-tutorial/static/css/main.33703357.chunk.css"
+  },
+  {
+    "revision": "4f0caff34d1cd5cd9cbf0d9f3ed472c6",
+    "url": "/react-tutorial/index.html"
+  }
+];
